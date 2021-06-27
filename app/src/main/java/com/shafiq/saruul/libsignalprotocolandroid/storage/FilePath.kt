1a059955e4182fec6786544089e272204476d943
Saruul Shafiq <saruul.s@gmail.com>
2021-06-27 11:06:19 +0200

    Added storage handler.

---
+++ app/src/main/java/com/shafiq/saruul/libsignalprotocolandroid/storage/FilePath.kt
@@ -0,0 +1,17 @@
+package com.shafiq.saruul.libsignalprotocolandroid.storage
+
+import android.content.Context
+
+class FilePath constructor(context: Context, vararg identifiers: String) {
+
+    val filePath: String
+
+    init {
+        var filePath = context.filesDir.absolutePath
+        for (identifier in identifiers) {
+            filePath += "/"
+            filePath += identifier
+        }
+        this.filePath = filePath
+    }
+}
--- app/src/main/java/com/shafiq/saruul/libsignalprotocolandroid/ui/main/di/MainModule.kt
+++ app/src/main/java/com/shafiq/saruul/libsignalprotocolandroid/ui/main/di/MainModule.kt
@@ -1,17 +1,34 @@
 package com.shafiq.saruul.libsignalprotocolandroid.ui.main.di
 
 import androidx.lifecycle.ViewModel
+import com.google.gson.Gson
 import com.shafiq.saruul.libsignalprotocolandroid.di.ViewModelKey
+import com.shafiq.saruul.libsignalprotocolandroid.storage.ExampleStorageHandler
+import com.shafiq.saruul.libsignalprotocolandroid.storage.StorageHandler
 import com.shafiq.saruul.libsignalprotocolandroid.ui.main.MainViewModel
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.multibindings.IntoMap
 
 @Module
 abstract class MainModule {
 
+    @Module
+    companion object {
+
+        @JvmStatic
+        @Provides
+        fun provideGson(): Gson {
+            return Gson()
+        }
+    }
+
     @Binds
     @IntoMap
     @ViewModelKey(MainViewModel::class)
     abstract fun bindViewModel(viewModel: MainViewModel): ViewModel
+
+    @Binds
+    abstract fun bindStorageHandler(exampleStorageHandler: ExampleStorageHandler): StorageHandler
 }
