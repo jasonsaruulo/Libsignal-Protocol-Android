dda37fe39ce23e8259d6f6f8254d10c8fde98f21
Saruul Shafiq <saruul.s@gmail.com>
2021-07-11 20:28:04 +0200

    Added generating/getting uuid to encryption handler.

--- app/src/main/java/com/shafiq/saruul/libsignalprotocolandroid/handlers/encryption/ExampleEncryptionHandler.kt
+++ app/src/main/java/com/shafiq/saruul/libsignalprotocolandroid/handlers/encryption/ExampleEncryptionHandler.kt
@@ -3,6 +3,7 @@ package com.shafiq.saruul.libsignalprotocolandroid.handlers.encryption
 import com.shafiq.saruul.libsignalprotocolandroid.handlers.HandlerModule.Companion.HOME_DIRECTORY
 import com.shafiq.saruul.libsignalprotocolandroid.handlers.storage.FilePath
 import com.shafiq.saruul.libsignalprotocolandroid.handlers.storage.StorageHandler
+import java.util.UUID
 import javax.inject.Inject
 import javax.inject.Named
 
@@ -15,7 +16,8 @@ class ExampleEncryptionHandler @Inject constructor(
     companion object {
         private const val ENCRYPTION_DIRECTORY = "encryption"
 
-        private const val ENCRYPTION_DATA_UPLOADED = "encryptionDataUploaded"
+        private const val UUID_FILE = "uuid"
+        private const val ENCRYPTION_DATA_UPLOADED_FILE = "encryptionDataUploaded"
     }
 
     private val filePath: FilePath = FilePath(homeDirectory, ENCRYPTION_DIRECTORY)
@@ -24,17 +26,27 @@ class ExampleEncryptionHandler @Inject constructor(
         if (readEncryptionDataUploaded()) {
             // TODO: Check whether pre keys need update
         } else {
+            val uuid = getOrGenerateUuid()
             // TODO: Create identity key pair, registration id, signed pre key, serialised pre key and store on server
             saveEncryptionDataUploaded()
         }
     }
 
+    private fun getOrGenerateUuid(): String {
+        storageHandler.readFile(filePath, UUID_FILE)?.let {
+            return it
+        }
+        val uuid = UUID.randomUUID()
+        storageHandler.saveToFile(filePath, UUID_FILE, uuid.toString())
+        return uuid.toString()
+    }
+
     private fun saveEncryptionDataUploaded() {
-        storageHandler.saveToFile(filePath, ENCRYPTION_DATA_UPLOADED, true.toString())
+        storageHandler.saveToFile(filePath, ENCRYPTION_DATA_UPLOADED_FILE, true.toString())
     }
 
     private fun readEncryptionDataUploaded(): Boolean {
-        storageHandler.readFile(filePath, ENCRYPTION_DATA_UPLOADED)?.let { data ->
+        storageHandler.readFile(filePath, ENCRYPTION_DATA_UPLOADED_FILE)?.let { data ->
             return data.toBoolean()
         }
         return false
